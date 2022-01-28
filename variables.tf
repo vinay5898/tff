1de189dbf9fe7f1b69b4faae117010cbe1dfebec
vinay5898 <69567526+vinay5898@users.noreply.github.com>
2022-01-28 14:47:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
-  default = "AKIAVFBR3CHWJZY2S3UV"
+  default = "AKIAVF42YRR5Y4VUAIGA"
   }
 variable "secret_key" {
-  default = "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"
+  default = "TFH7eyB4uB27OXTPXoK+9uR68FHL+vm2IE6+9ZGR"
   }
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-1"
   }
